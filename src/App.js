520ee78edcf72d6db6b0aa5851c84d42509afb7b
JoSujin-cando <81570268+JoSujin-cando@users.noreply.github.com>
2023-07-18 17:04:33 +0900

    final
    합치기 전 최종 코드

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Test from "./pages/Test";
+import Korean from "./pages/Korean";
+import Asian from "./pages/Asian";
+import Western from "./pages/Western";
+import Alcohol from "./pages/Alcohol";
+import Dessert from "./pages/Dessert";
+import Brunch from "./pages/Brunch";
 
 function App() {
   return (
@@ -11,6 +17,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/test" element={<Test />} />
+          <Route path="/korean" element={<Korean />} />
+          <Route path="/asian" element={<Asian />} />
+          <Route path="/western" element={<Western />} />
+          <Route path="/alcohol" element={<Alcohol />} />
+          <Route path="/dessert" element={<Dessert />} />
+          <Route path="/brunch" element={<Brunch />} />
           <Route />
         </Routes>
       </BrowserRouter>
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,8 +1,63 @@
+import noonsong from "../img/noonsong.png";
+import { useNavigate } from "react-router-dom";
+import "../component/Test.css";
+
 const Test = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Test</h1>
-      <p>테스트페이지</p>
+    <div className="test">
+      <h2>여러분의 취향에 맞춰 테스트를 진행해 주세요</h2>
+      <div className="btn">
+        <button
+          style={{ backgroundColor: "#9bb9da" }}
+          onClick={() => {
+            navigate("/korean");
+          }}
+        >
+          한식
+        </button>
+        <button
+          style={{ backgroundColor: "#9bb9da" }}
+          onClick={() => {
+            navigate("/asian");
+          }}
+        >
+          아시안
+        </button>
+        <button
+          style={{ backgroundColor: "#9bb9da" }}
+          onClick={() => {
+            navigate("/western");
+          }}
+        >
+          양식
+        </button>
+        <button
+          style={{ backgroundColor: "#9bb9da" }}
+          onClick={() => {
+            navigate("/alcohol");
+          }}
+        >
+          술집
+        </button>
+        <button
+          style={{ backgroundColor: "#9bb9da" }}
+          onClick={() => {
+            navigate("/dessert");
+          }}
+        >
+          디저트
+        </button>
+        <button
+          style={{ backgroundColor: "#9bb9da" }}
+          onClick={() => {
+            navigate("/brunch");
+          }}
+        >
+          브런치
+        </button>
+      </div>
+      <img src={noonsong} width={450} />
     </div>
   );
 };
