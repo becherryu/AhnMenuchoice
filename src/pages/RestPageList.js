871e2dfe4667d611d85ce51cf8f51956c59c1337
Yerim Kim <127467946+Yearmi@users.noreply.github.com>
2023-07-31 21:13:01 +0900

    수정2

--- src/pages/RestPageList.js
+++ src/pages/RestPageList.js
@@ -4,7 +4,8 @@ import { IoIosArrowBack } from "react-icons/io";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./RestPageList.css";
+import "./css/RestPageList.css";
+
 import styles from "./Result.module.css";
 
 const restaurantData = [
