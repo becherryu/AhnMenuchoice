f73852749e3258ca0f5df2f02a6c48ba12d5cc6e
ujkim <A1ienalnK@gmail.com>
2023-08-03 00:54:46 +0900

    버튼 삭제, 수정, 정렬

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -3,42 +3,27 @@ import { Link } from "react-router-dom";
 
 import "../component/Result.css";
 import styles from "../component/Result.module.css";
-
-import { IoIosArrowBack } from "react-icons/io";
+import "../component/Home.css";
 
 import resultsmp from "../img/result_sample.png";
 
 const ResultPage = () => {
+
   return (
     <div className="Result">
-      <div className="back" style={{ textAlign: "left" }}>
-        <IoIosArrowBack size="30" color="white" />
-      </div>
+      <br/>
       <h1 className={styles.title}>당신의 선택 결과</h1>
 
       <div>
         <img src={resultsmp} />
       </div>
 
-      <button
-        className={styles.btn_shadow}
-        style={{ backgroundColor: "white", color: "black" }}
-      >
-        위치 보러가기
-      </button>
-      <br />
-      <button
-        className={styles.btn_shadow}
-        style={{ backgroundColor: "#1F60A9" }}
-      >
+      <button id="testBtn">
         다시 선택하기
       </button>
       <br />
       <Link to="/resultpagelist">
-        <button
-          className={styles.btn_shadow}
-          style={{ backgroundColor: "#0C2D83" }}
-        >
+        <button id="listBtn">
           가게 보러가기
         </button>
       </Link>
