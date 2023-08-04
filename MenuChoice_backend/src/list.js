9f0091723e5f5b8418dfe96ce8a47d8472255598
ajeong <ajung7038@naver.com>
2023-08-04 18:01:23 +0900

    쿼리 수정 중

--- MenuChoice_backend/src/list.js
+++ MenuChoice_backend/src/list.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-var db = require("./db.js");
+var db = require("../db.js");
 var conn = db.init();
 
-//const koreanRouter = require("./korean-db");
+const koreanRouter = require("./korean-db");
 
 // /Korean 엔드포인트에 koreanRouter 라우터를 등록
-//app.use("/Korean", koreanRouter);
+app.use("/Korean", koreanRouter);
 
 // MySQL 연결
 db.connect(conn);
@@ -32,76 +32,76 @@ app.get("/", function (req, res) {
 //
 //
 //
-// //
-// // 가게 리스트 페이지
-// app.get("/resultpagelist", function (req, res) {
-//   // 버튼 선택에 따른 sql문 구현
-//   let sql;
+//
+// 가게 리스트 페이지
+app.get("/resultpagelist", function (req, res) {
+  // 버튼 선택에 따른 sql문 구현
+  let sql;
 
-//   if (req.query.type == 0) {
-//     sql = "SELECT store.title FROM store"; // 전체
-//   } else if (req.query.type >= 1 && req.query.type <= 6) {
-//     // 한식, 아시안, 양식, 술집, 디저트, 브런치
-//     sql = `
-//     SELECT store.title
-//     FROM store
-//     JOIN \`id-store_id\` ON store.id = \`id-store_id\`.id
-//     WHERE \`id-store_id\`.store_type_id = ${req.query.type}
-//     `;
-//   } else {
-//     // 적절하지 않은 버튼 선택에 대한 예외처리
-//     res.status(400).send("잘못된 버튼 선택");
-//     return;
-//   }
+  if (req.query.type == 0) {
+    sql = "SELECT store.title FROM store"; // 전체
+  } else if (req.query.type >= 1 && req.query.type <= 6) {
+    // 한식, 아시안, 양식, 술집, 디저트, 브런치
+    sql = `
+    SELECT store.title
+    FROM store
+    JOIN \`id-store_id\` ON store.id = \`id-store_id\`.id
+    WHERE \`id-store_id\`.store_type_id = ${req.query.type}
+    `;
+  } else {
+    // 적절하지 않은 버튼 선택에 대한 예외처리
+    res.status(400).send("잘못된 버튼 선택");
+    return;
+  }
 
-//   // 쿼리 실행
-//   conn.query(sql, (err, results) => {
-//     if (err) {
-//       console.error("쿼리 실행 에러 : " + err);
-//       res.status(500).send("쿼리 실행 오류");
-//     } else {
-//       // 쿼리 결과를 res.send로 보냄
-//       res.send(results);
-//     }
-//   });
-// });
+  // 쿼리 실행
+  conn.query(sql, (err, results) => {
+    if (err) {
+      console.error("쿼리 실행 에러 : " + err);
+      res.status(500).send("쿼리 실행 오류");
+    } else {
+      // 쿼리 결과를 res.send로 보냄
+      res.send(results);
+    }
+  });
+});
 
-// //
-// //
-// //
-// //
-// // 테스트 페이지
-// app.get("/Korean", function (req, res) {
-//   // 버튼 선택에 따른 sql문 구현
-//   let sql;
+//
+//
+//
+//
+// 테스트 페이지
+app.get("/Korean", function (req, res) {
+  // 버튼 선택에 따른 sql문 구현
+  let sql;
 
-//   if (req.query.type >= 1 && req.query.type <= 6) {
-//     // 한식, 아시안, 양식, 술집, 디저트, 브런치
-//     sql = `
-//     SELECT store.title
-//     FROM store
-//     JOIN \`id-store_id\` ON store.id = \`id-store_id\`.id
-//     WHERE \`id-store_id\`.store_type_id = ${req.query.type}
-//   `;
-//   } else {
-//     // 적절하지 않은 버튼 선택에 대한 예외처리
-//     res.status(400).send("잘못된 버튼 선택");
-//     return;
-//   }
+  if (req.query.type >= 1 && req.query.type <= 6) {
+    // 한식, 아시안, 양식, 술집, 디저트, 브런치
+    sql = `
+    SELECT store.title
+    FROM store
+    JOIN \`id-store_id\` ON store.id = \`id-store_id\`.id
+    WHERE \`id-store_id\`.store_type_id = ${req.query.type}
+  `;
+  } else {
+    // 적절하지 않은 버튼 선택에 대한 예외처리
+    res.status(400).send("잘못된 버튼 선택");
+    return;
+  }
 
-//   // 쿼리 실행
-//   conn.query(sql, (err, results) => {
-//     if (err) {
-//       console.error("쿼리 실행 에러 : " + err);
-//       res.status(500).send("쿼리 실행 오류");
-//     } else {
-//       // // 쿼리 결과를 res.send로 보냄
-//       // res.send(results);
+  // 쿼리 실행
+  conn.query(sql, (err, results) => {
+    if (err) {
+      console.error("쿼리 실행 에러 : " + err);
+      res.status(500).send("쿼리 실행 오류");
+    } else {
+      // // 쿼리 결과를 res.send로 보냄
+      // res.send(results);
 
-//       // 쿼리 결과를 /Korean/example로 보냄
-//       res.redirect("/Korean/example");
-//     }
-//   });
-// });
+      // 쿼리 결과를 /Korean/example로 보냄
+      res.redirect("/Korean/example");
+    }
+  });
+});
 
 app.listen(3000);
